975172e913400dda6d23c831067d52f06bc2e4ef
AbooTuraab <aliyulekan@gmail.com>
2022-10-31 16:12:56 +0100

    initial commmit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <div>
+      
+
+       {/*links section*/}
+       <link className="btn_zuri "></link>
+      {/*links section*/}
+    </div>
+  );
+}
+
+export default App;
