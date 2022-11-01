1ebdf8b940631191f269d1511bf1881c7bbe42ea
AbooTuraab <aliyulekan@gmail.com>
2022-11-01 03:58:44 +0100

    final commit

--- src/components/Links.js
+++ src/components/Links.js
@@ -15,7 +15,8 @@ function Links() {
     <div class="panel-heading" role="tab" id="headingTwo">
      
         <a role="button"  href="https://training.zuri.team/" >
-          <div class="bt_zuri btn btn-danger btn-outline btn-lg">Zuri team</div>
+          <div class="btn btn-danger btn-outline btn-lg"
+          id="btn_zuri">Zuri team</div>
         </a>
      </div>
     </div>
@@ -25,9 +26,12 @@ function Links() {
     <div class="panel-heading" role="tab" id="headingTwo">
      
         <a role="button"  href="https://books.zuri.team/" >
-          <div class="books btn btn-danger btn-outline btn-lg">Zuri Books</div>
+          <div class="btn btn-danger btn-outline btn-lg"
+          id="books">Zuri Books</div>
         </a>
-        
+        <p>Are you an upcoming developer or product designer? if yes, I have
+        a good news for you, all books you need to succeed in becoming 
+        a top rated designer or developer are all available here</p>
      </div>
     </div>
   
@@ -36,7 +40,8 @@ function Links() {
     <div class="panel-heading" role="tab" id="headingTwo">
      
         <a role="button"  href="https://books.zuri.team/python-for-beginners?ref_id=adenijialiyuolamilekan" >
-          <div class="books_python title btn btn-danger btn-outline btn-lg">Python Books</div>
+          <div class="title btn btn-danger btn-outline btn-lg"
+          id="book_python">Python Books</div>
         </a>
         <p>This is my book called python for beginners, I wrote this book
           to guide every programmer who wants to learn python programming language
@@ -48,8 +53,11 @@ function Links() {
     <div class="panel-heading" role="tab" id="headingTwo">
      
         <a role="button"  href="https://background.zuri.team" >
-          <div class="pitching title btn btn-danger btn-outline btn-lg">Background Check for Coders</div>
+          <div class="title btn btn-danger btn-outline btn-lg"
+          id="pitch">Background Check for Coders</div>
         </a>
+        <p>Getting experienced coders for your project  has never gotten easier,
+        at heritage consult, we are ready to connect you with the finest developers </p>
      </div>
     </div>
   
@@ -59,12 +67,13 @@ function Links() {
     <div class="panel-heading" role="tab" id="headingTwo">
      
         <a role="button"  href="https://books.zuri.team/" >
-          <div class="book_design title btn btn-danger btn-outline btn-lg">Design Books</div>
+          <div class="title btn btn-danger btn-outline btn-lg"
+          id="book_design">Design Books</div>
         </a>
         <p>
-The HNG Design Rules Book gives you the tips &
- guidelines you need to create truly professional designs, 
-by addressing errors that could you get you disqualified.</p>
+            The HNG Design Rules Book gives you the tips &
+            guidelines you need to create truly professional designs, 
+            by addressing errors that could you get you disqualified.</p>
      </div>
     </div>
   
@@ -73,6 +82,8 @@ by addressing errors that could you get you disqualified.</p>
    </div>
     </div>
   </div>
+
+
 
          </>
     )
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,3 +1,5 @@
+import myImage from "./linkedin.jpg"
+
 
 function Profile() {
     return(
@@ -8,8 +10,13 @@ function Profile() {
 
         </div>
       </div>
-      <ul class="text" id="twitter">@AbooTuraab</ul>
+      <div>
 
+      <img className='myimage' id='profile__img' src={myImage} alt="profile-pic" />
+
+      </div>
+      <ul class="text" id="twitter">@AbooTuraab</ul>
+      <ul class="text" id="slack" hidden>adenijialiyuolamilekan</ul>
        </>
     );
 }
